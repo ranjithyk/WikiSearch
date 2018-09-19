2f39dc2101149c0015b418974225c6fdfb7f4ab6
Ranjith Y K <Ranjithyk.yk@gmail.com>
2018-09-19 08:43:08 +0530

    Changed content url to mobile page url

--- app/src/main/java/com/example/wikisearch/wikisearch/Constants.java
+++ app/src/main/java/com/example/wikisearch/wikisearch/Constants.java
@@ -3,5 +3,5 @@ package com.example.wikisearch.wikisearch;
 public class Constants {
 
     public static final String SEARCH_BASE_URL = "https://en.wikipedia.org/w/api.php?";
-    public static final String CONTENT_BASE_URL = "https://en.wikipedia.org/?";
+    public static final String CONTENT_BASE_URL = "https://en.m.wikipedia.org/?";
 }
